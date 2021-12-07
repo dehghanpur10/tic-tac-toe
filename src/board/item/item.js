22f4d2186b2f1a27213bf5d39e9baae403adf00e
Mohammad Dehghanpour <m.dehghanpour10@gmail.com>
2021-12-08 00:09:42 +0330

    Fix lint error

--- src/board/item/item.js
+++ src/board/item/item.js
@@ -1,5 +1,5 @@
 import Button from '@mui/material/Button';
-import {createTheme, Typography} from "@mui/material";
+import {createTheme} from "@mui/material";
 import {ThemeProvider} from "@emotion/react";
 import classes from './item.module.css'
 
